260478723b77af9489ae762819ad18dd66431517
Justin Harrison <jth9819@gmail.com>
2022-05-24 15:44:09 -0500

    Complete desktop view navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+// import React, { useState } from 'react';
+// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import Logo from '../../assets/logo.svg';
+import './navbar.css';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  // const [toggleMenu, setToggleMenu] = useState(false);
 
-export default Navbar
+  return (
+    <div className="navbar">
+      <div className="navbar-links">
+        <div className="navbar-links__logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="navbar-links__container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className='navbar-sign'>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      {/* <div className="navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+
+        )}
+      </div>   */}
+    </div>
+  );
+};
+
+export default Navbar;
