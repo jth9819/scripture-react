8faf7b90bfd57da6e5f468ec9786746d8fcf2846
Justin Harrison <jth9819@gmail.com>
2022-05-25 10:56:19 -0500

    Complete navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-// import React, { useState } from 'react';
-// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import Logo from '../../assets/logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
-  // const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     <div className="navbar">
@@ -25,15 +24,27 @@ const Navbar = () => {
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
-      {/* <div className="navbar-menu">
+      <div className="navbar-menu">
         {toggleMenu
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
         }
         {toggleMenu && (
-
+          <div className="navbar-menu__container scale-up-center">
+            <div className="navbar-menu__container-links">
+              <p><a href="#home">Home</a></p>
+              <p><a href="#wgpt3">What is GPT3?</a></p>
+              <p><a href="#possibility">Open AI</a></p>
+              <p><a href="#features">Case Studies</a></p>
+              <p><a href="#blog">Library</a></p>
+            </div>
+            <div className="navbar-menu__container-links-sign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
+          </div>
         )}
-      </div>   */}
+      </div>  
     </div>
   );
 };
